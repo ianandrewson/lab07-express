20d06d585eb794454af7a877325c416a6416017a
Ian Andrewson <ianandrewson@gmail.com>
2019-12-10 15:39:29 -0800

    test written for route to get recipe by ID, test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -93,4 +93,34 @@ describe('app routes', () => {
         });
       });
   });
+
+  it('gets a single recipe by id', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+    return request(app)
+      .get(`/api/v1/recipes/${recipe._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: recipe._id.toString(),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          __v: 0
+        });
+      });
+  });
+  it('deletes a single recipe by id', () => {
+
+  });
 });
